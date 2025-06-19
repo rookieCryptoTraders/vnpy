57448519946b0960c3f953b43c7c671074dd120a
Ch1nZ <chzao0928@gmail.com>
2025-06-19 23:03:43 +0800

    fix: update .gitignore to correct ta_lib directory path and remove unused factor files

--- tests/ftest.py
+++ tests/ftest.py
@@ -1,29 +1,12 @@
-# @Project  : 20240720
-# @FilePath : vnpy/tests
-# @File     : real_test.py
-# @Time     : 2025/1/21 19:25
-# @Author   : ChenZ
-# @Email    : czhao0928@gmail.com
-# @Description:
+import polars as pl
+import polars_talib as plta
 
-from datetime import datetime
+df = pl.DataFrame({
+    "Close": [10, 12, 15, 13, 16, 18, 20, 22, 25, 23]
+})
 
-from vnpy.factor.engine import FactorEngine
-#from vnpy.app.factor_maker import FactorMakerApp
-from vnpy.event import EventEngine
-from vnpy.trader.engine import MainEngine
-#from vnpy.strategy.examples.test_strategy_template import TestStrategyTemplate
+df = df.with_columns(
+    plta.bbands(pl.col("Close"))
+)
 
-
-if __name__ == '__main__':
-    event_engine = EventEngine()
-    main_engine = MainEngine(event_engine)
-    main_engine.write_log("Main engine created successfully")
-    main_engine.vt_symbols = ['btcusdt.BINANCE', 'ethusdt.BINANCE']
-
-    # start factor engine
-    factor_maker_engine: FactorEngine = main_engine.add_engine(FactorEngine)
-    factor_maker_engine.init_engine()
-    factor_maker_engine.init_memory(True)
-    factor_maker_engine.execute_calculation(dt=datetime.now())
-    factor_maker_engine.close()
+print(df)
